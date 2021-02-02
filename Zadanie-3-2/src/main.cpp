6c9c924afba2be4541e650a8f57b839a8fd2c1b3
„Aleksandra <aleksandra.romanowska@student.wat.edu.pl>
2021-02-02 15:41:44 +0100

    Wykonano zadanie 3.2.1 – pojedynczy wyświetlacz

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -4,6 +4,7 @@ uint8_t cyfra[10] = {0b0111111, 0b0000110,0b1001111,0b1100110,
 0b1101101,0b0000111,0b1111111,0b1101111};
 void dec_to_7seg(uint8_t);
 void setup() {
+
    for (uint8_t i= 0; i < 8; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
@@ -15,7 +16,7 @@ void setup() {
   digitalWrite(8, HIGH);
 }
 void loop() {
-    for (int i =0; i < 10; i++) {
+   for (int i =0; i < 10; i++) {
     dec_to_7seg(i);
     delay(1000);
   }
@@ -28,4 +29,4 @@ void dec_to_7seg(uint8_t dec) {
     else
     digitalWrite(i, HIGH);
   }
-}
+} 
